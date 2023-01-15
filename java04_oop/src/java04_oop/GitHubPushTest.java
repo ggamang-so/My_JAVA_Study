404e2ee3b338ae3b6c323b0fe606cdccd1fbea46
ggamangso1 <ggamangso@gmail.com>
2023-01-15 15:50:14 +0900

    eclipes push test

---
+++ java04_oop/src/java04_oop/GitHubPushTest.java
@@ -0,0 +1,14 @@
+package java04_oop;
+
+public class GitHubPushTest {
+
+	public GitHubPushTest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
