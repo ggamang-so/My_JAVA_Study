ad8433aeccb63b5cb67b2d8d20f340de3f0b2384
ggamangso1 <ggamangso@gmail.com>
2023-03-12 01:08:32 +0900

    beakjoon 3273 done

--- beakjoon/src/beakjoon/Y_Ex3273.java
+++ beakjoon/src/beakjoon/Y_Ex3273.java
@@ -3,29 +3,39 @@ package beakjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Ex3273 {
+public class Y_Ex3273 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int count=0;
         int n = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
-        int[] sum = new int[n+1];
+        int[] arr = new int[n+1];
         for (int i = 1; i <= n; i++) {
-            sum[i] = Integer.parseInt(st.nextToken());
+            arr[i] =Integer.parseInt(st.nextToken());
         }
         int k = Integer.parseInt(new StringTokenizer(br.readLine()).nextToken());
         int left = 1;
-        int right = 2;
+        int right = n;
+        Arrays.sort(arr);
         while(true){
-
-
+            if(left==right) break;
+            if(arr[left]+arr[right]<k) {
+                left++;
+                continue;
+            }
+            if(arr[left]+arr[right]==k){
+                count++;
+                right--;
+                continue;
+            }
+            if(arr[left]+arr[right]>k){
+                right--;
+            }
         }
-
         System.out.println(count);
-
-
     }
 }
